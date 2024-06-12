c7e7b2414d49ead18b7f0dc0cf5404c0be90c504
Vishnu M <vyshnav.m345@gmail.com>
2024-06-12 20:04:55 +0530

    setting the backed address as elastic IP

--- src/config/index.js
+++ src/config/index.js
@@ -1,3 +1,5 @@
-export const API_URL = 'http://127.0.0.1'  
+// export const API_URL = 'http://127.0.0.1'  
+export const API_URL = 'https://54.165.33.141'
 // export const API_URL = 'http://localhost:8000';
 export const WS_link = "ws://127.0.0.1:8000";
+// export const WS_link = "ws://54.165.33.141";
