901749b66f5f7bd90a03283df2a347169d4715c1
Vishnu M <vyshnav.m345@gmail.com>
2024-06-13 12:42:09 +0530

    updated api  url and ws to point at subdomain

--- src/config/index.js
+++ src/config/index.js
@@ -1,5 +1,8 @@
-export const API_URL = 'https://54.165.33.141'
-export const WS_link = "wss://54.165.33.141";
+export const API_URL = "https://www.server.getfittoday.xyz";
+export const WS_link = "wss://www.server.getfittoday.xyz";
+
+// export const API_URL = 'https://54.165.33.141'
+// export const WS_link = "wss://54.165.33.141";
 
 
 // export const WS_link = "ws://127.0.0.1:8000";
