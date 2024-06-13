6b2f34eb677152e6cd6bb273321869b009100be2
Vishnu M <vyshnav.m345@gmail.com>
2024-06-13 10:48:03 +0530

    updated the trainer info page

--- src/components/trainer/TrainerData.js
+++ src/components/trainer/TrainerData.js
@@ -1,8 +1,4 @@
 import React, { useEffect } from 'react'
-import img1 from "assets/hero_image2.jpg";
-import img2 from "assets/TrainerBaground.jpg";
-import img3 from "assets/kris gethin.jpg";
-import img4 from "assets/heroImage.jpg";
 import { useDispatch, useSelector } from 'react-redux';
 import { getTrainerByID } from 'features/trainer';
 import { API_URL } from 'config';
@@ -10,10 +6,16 @@ import { API_URL } from 'config';
 const TrainerData = ({id}) => {
     const { selectedTrainer } = useSelector((state) => state.trainer);
     const dispatch = useDispatch()
+    const [activeTab, setActiveTab] = useState("about");
 
     useEffect(()=>{
         dispatch(getTrainerByID(id));
     },[])
+
+    const handleTabClick = (tab) => {
+      setActiveTab(tab);
+    };
+
     return (
       <div className=" px-2 container mx-auto py-16 md:px-10 ">
         <div className="mt-8 lg:flex grid grid-cols-1 lg:grid-cols-2 gap-5">
@@ -34,36 +36,66 @@ const TrainerData = ({id}) => {
             <div className="bg-gray-200 py-4 px-6 rounded-lg">
               <ul className="flex space-x-3">
                 <li>
-                  <a
+                  {/* <a
                     className="py-2 px-4 bg-white text-primary font-semibold rounded-full transition duration-300 hover:bg-primary hover:text-white"
                     href="#tab-1"
                   >
                     About
-                  </a>
+                  </a> */}
+                  <button
+                    className={`py-2 px-4 font-semibold rounded-full transition duration-300 ${
+                      activeTab === "about"
+                        ? "bg-primary text-white"
+                        : "bg-white text-primary"
+                    }`}
+                    onClick={() => handleTabClick("about")}
+                  >
+                    About
+                  </button>
                 </li>
                 <li>
-                  <a
+                  {/* <a
                     className="py-2 px-4 bg-white text-primary font-semibold rounded-full transition duration-300 hover:bg-primary hover:text-white"
                     href="#tab-2"
                   >
                     Mission
-                  </a>
+                  </a> */}
+                  <button
+                    className={`py-2 px-4 font-semibold rounded-full transition duration-300 ${
+                      activeTab === "mission"
+                        ? "bg-primary text-white"
+                        : "bg-white text-primary"
+                    }`}
+                    onClick={() => handleTabClick("mission")}
+                  >
+                    Mission
+                  </button>
                 </li>
                 <li>
-                  <a
+                  {/* <a
                     className="py-2 px-4 bg-white text-primary font-semibold rounded-full transition duration-300 hover:bg-primary hover:text-white"
                     href="#tab-3"
                   >
                     Vision
-                  </a>
+                  </a> */}
+                  <button
+                    className={`py-2 px-4 font-semibold rounded-full transition duration-300 ${
+                      activeTab === "vision"
+                        ? "bg-primary text-white"
+                        : "bg-white text-primary"
+                    }`}
+                    onClick={() => handleTabClick("vision")}
+                  >
+                    Vision
+                  </button>
                 </li>
               </ul>
-              <div className="mt-4">
+              {/* <div className="mt-4">
                 <div id="tab-1" className="tab-content">
-                  <h5 className="text-lg font-semibold">Lorem Ipsum 1</h5>
-                  <p className="text-lg mb-4">
-                    {selectedTrainer?.about}
-                  </p>
+                  <h5 className="text-lg font-semibold">
+                    Transforming Lives through Fitness
+                  </h5>
+                  <p className="text-lg mb-4">{selectedTrainer?.about}</p>
                   <a
                     className="btn btn-primary py-2 px-4 rounded-lg transition duration-300 hover:bg-primary hover:text-white"
                     href="#"
@@ -100,6 +132,54 @@ const TrainerData = ({id}) => {
                     Read More
                   </a>
                 </div>
+              </div> */}
+              <div className="mt-4">
+                {activeTab === "about" && (
+                  <div id="tab-1" className="tab-content">
+                    <h5 className="text-lg font-semibold">
+                      Transforming Lives through Fitness
+                    </h5>
+                    <p className="text-lg mb-4">{selectedTrainer?.about}</p>
+                    <a
+                      className="btn btn-primary py-2 px-4 rounded-lg transition duration-300 hover:bg-primary hover:text-white"
+                      href="#"
+                    >
+                      Read More
+                    </a>
+                  </div>
+                )}
+                {activeTab === "mission" && (
+                  <div id="tab-2" className="tab-content">
+                    <h5 className="text-lg font-semibold">Our Mission</h5>
+                    <p className="text-lg mb-4">
+                      Our mission is to inspire and empower individuals to
+                      achieve their best selves through dedicated training,
+                      personalized nutrition, and holistic wellness strategies.
+                    </p>
+                    <a
+                      className="btn btn-primary py-2 px-4 rounded-lg transition duration-300 hover:bg-primary hover:text-white"
+                      href="#"
+                    >
+                      Read More
+                    </a>
+                  </div>
+                )}
+                {activeTab === "vision" && (
+                  <div id="tab-3" className="tab-content">
+                    <h5 className="text-lg font-semibold">Our Vision</h5>
+                    <p className="text-lg mb-4">
+                      Our vision is to be the leading source of fitness
+                      transformation, creating a healthier and more active world
+                      through our innovative programs and community support.
+                    </p>
+                    <a
+                      className="btn btn-primary py-2 px-4 rounded-lg transition duration-300 hover:bg-primary hover:text-white"
+                      href="#"
+                    >
+                      Read More
+                    </a>
+                  </div>
+                )}
               </div>
             </div>
           </div>
